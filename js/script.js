31b1f0e80821d1022ac3e39143cfc8eab531d44c
Jonathan Ayala <126Jonathanayala@frhsd.com>
2025-02-12 12:56:04 -0500

    lkj

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function generateStory() {
     const place = document.getElementById("place").value;
     const food = document.getElementById("food").value;
     
-    const story = `One day, SpongeBob woke up feeling ${adjective}. He grabbed his pet ${noun} and decided to ${verb} all the way to ${place}. When he got there, he saw Patrick eating a huge ${food}! "That looks delicious!" SpongeBob said.`;
+    const story = `One day, SpongeBob woke up feeling ${adjective}. He grabbed his pet ${noun} and decided to ${verb} all the way to the ${place}. When he got there, he saw Patrick eating a huge ${food}! "That looks delicious!" SpongeBob said.`;
     
     document.getElementById("story").innerText = story;
 }
